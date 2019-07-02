f1ce881170bd66966737958ac01949a3611b8cdd
albertoisaac.pico <albertoisaac.pico@bbva.com>
2019-07-02 11:05:15 -0500

    Carga de todo el curso

---
+++ CodeWars/Python/highest_and_lowest.py
@@ -0,0 +1,10 @@
+def high_and_low(numeros):
+    numeros2 = numeros.split(" ")
+    mayor = int(numeros[0])
+    menor = int(mnumeros[0])
+    if numeros2.len == 1:
+        return numeros
+    else:
+        for x in numeros.len:
+            
+    
