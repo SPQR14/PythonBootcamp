3b90a66f0bb8115cdd2edf034205d08298f3be4d
albertoisaac.pico <albertoisaac.pico@bbva.com>
2019-07-30 10:53:18 -0500

    hasta files

---
+++ CodeWars/Python/Windows.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <time.h>
+
+int main(){
+    printf("Examinando en busca de problemas con el equipo\n");
+    sleep(1000000000000000);
+    printf("No se hallaron problemas en el equipo");
+    return 0;
+}
