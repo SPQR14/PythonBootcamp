cc772f9ce839ce25d78c8894b8fe01a3a21f7b7d
albertoisaac.pico <albertoisaac.pico@bbva.com>
2019-08-06 17:23:44 -0500

    narcisista

--- CodeWars/Python/numero narcisista.py
+++ CodeWars/Python/numero narcisista.py
@@ -1,2 +1,13 @@
-def narciso( value ):
-    
+def narcissistic( value ):
+    cad = str(value)
+    lst_num = [int(i) for i in str(cad)]
+    cifras = len(lst_num)
+    for i in range(0, len(lst_num)):
+        lst_num[i] = (lst_num[i] ** cifras)
+    if value == (sum(lst_num)):
+        return True
+    else:
+        return False
+
+
+print(narcissistic(153))
