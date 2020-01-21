acd8e0e33e4b335af942e5bd617ea96b045d6de0
albertoisaac.pico <albertoisaac.pico@bbva.com>
2020-01-21 09:53:55 -0600

    De regreso... :O

--- CodeWars/Python/parentesis correctos.py
+++ CodeWars/Python/parentesis correctos.py
@@ -1,62 +1,46 @@
 def valid_parentheses(string):
-    if len(string) <= 0:
-        return True
-    elif len(string) >= 100:
-        return True
-    else:
-        string = string.replace(" ", "")
-  
+    string = string.replace(" ", "")
     if len(string) % 2 != 0:
-        pass
-        #return False
-
-    if not string:
+        return False
+    elif not string:
         return True
     else:
         pila = []
         val = True
         car = None
-        for x in range(0,len(string) - 1):
+        for x in range(0,len(string)):
             if string[x] == '(':
                 pila.append(')')
-                continue
             elif string[x] == '[':
                 pila.append(']')
-                continue
             elif string[x] == '{':
                 pila.append('}')
-                continue
             elif string[x] == '<':
                 pila.append('>')
-                continue
             elif string[x] == ')':
                 car = pila.pop()
                 if car != ')':
                     val = False
                 else:
                     val = True
-                continue
             elif string[x] == ']':
                 car = pila.pop()
                 if car != ']':
                     val = False
                 else:
                     val = True
-                continue
             elif string[x] == '}':
                 car = pila.pop()
                 if car != '}':
                     val = False
                 else:
                     val = True
-                continue
             elif string[x] == '>':
                 car = pila.pop()
                 if car != '>':
                     val = False
                 else:
                     val = True
-                continue
             if not val:
                 break
         
@@ -64,6 +48,8 @@ def valid_parentheses(string):
             return True
         else:
             return False
+
+
 
 """
 def validacion(string):
@@ -118,5 +104,5 @@ def validacion(string):
             return False
 """
 
-print(valid_parentheses("(g)s(ox)m(s)"))
+print(valid_parentheses(""))
 
--- CodeWars/Python/people_in_the_bus.py
+++ CodeWars/Python/people_in_the_bus.py
@@ -2,13 +2,16 @@ def number(bus_stops):
     aux0 = []
     aux1 = []
     total = 0
-    for i in range(0, len(bus_stops) + 1):
+    for i in range(0, len(bus_stops)):
         aux0 = bus_stops[i]
         print(aux0)
         aux1 = bus_stops[i + 1]
         print(aux1)
         total += aux0[0] + aux1[0] - aux1[1]
         print(total)
-    return total
+    if total < 0:
+        return 0
+    else:
+        return total
 
 print(number([[10,0],[3,5],[5,8]]))
