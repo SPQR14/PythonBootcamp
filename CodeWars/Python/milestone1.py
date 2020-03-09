560cc2f8b9f970bc68ad63665ac88b7632c7bfa2
albertoisaac.pico <albertoisaac.pico@bbva.com>
2020-03-09 13:42:44 -0600

    milestone 1 completo

--- CodeWars/Python/milestone1.py
+++ CodeWars/Python/milestone1.py
@@ -11,52 +11,104 @@ tablero = [['1 ', '| ', '2 ', '| ', '3'],
            ['- ','- ' ,'- ', '- ' ,'- '], 
            ['7 ', '| ', '8 ', '| ', '9']]
 
-jugadas = {1: [[0][0]], 2: [[0][2]], 3: [[0][4]], 
-           4: [[2][0]], 5: [[2][2]], 6: [[2][4]],
-           7: [[4][0]], 8: [[4][0]], 9: [[4][0]]}
 
 tiradas = []
-jugador1 = []
-jugador2 = []
 turno = 1
-jugador = 0
+jugador = 'X'
+numero_jugador = 1
+
 
 def inputs(message, tiradas):
-    print("en este monento:")
-    print(tiradas)
     while(True):
         entrada = int(input(message))
         if (entrada >= 1 and entrada <= 9):
             if entrada in tiradas:
-                print("Wrong movement, try again!")
+                print("Intenta de nuevo")
+                os.system("pause")
+                os.system("CLS")
+                imprimir_tablero()
             else:
                 return entrada
         else:
-            print("Must be a number between 1 and 9")
+            print("Debe ser un número entre 1 y 9")
+            os.system("pause")
+            os.system("CLS")
+            imprimir_tablero()
 
 
 def imprimir_tablero():
-    print(f"turno del jugador: {jugador}",end='')
-    print(f"\t\tturno {turno}, restantes {9-turno}\n\n")
+    print(f"turno del jugador: {numero_jugador}, {jugador}",end='')
+    print(f"\t\tturno {turno}, restantes {10-turno}\n\n")
     for x in range(5):
         for y in range(5):
             print(tablero[x][y], end='')
         print()
 
 
-def actualiza_tablero(tiro):
-    tiro = str(tiro)
-    for item in jugadas:
-        if item == tiro:
-            pass           
+def switcher(item):
+    switcher = {
+        1: lambda: [[0],[0]],
+        2: lambda: [[0],[2]],
+        3: lambda: [[0],[4]],
+        4: lambda: [[2],[0]],
+        5: lambda: [[2],[2]],
+        6: lambda: [[2],[4]],
+        7: lambda: [[4],[0]],
+        8: lambda: [[4],[2]],
+        9: lambda: [[4],[4]]
+    }
+    des = switcher.get(item, 'invalid')
+    return des()
 
+
+def actualiza_tablero(tiro):
+    posicion = switcher(tiro)
+    if turno % 2 != 0:
+        tablero[posicion[0][0]][posicion[1][0]] = ('X ')
+    else:
+        tablero[posicion[0][0]][posicion[1][0]] = ('O ')    
+
+
+def selecciona_ganador():
+    horizontal1 = tablero[0][0] + tablero[0][2] + tablero[0][4]
+    horizontal2 = tablero[2][0] + tablero[2][2] + tablero[2][4]
+    horizontal3 = tablero[4][0] + tablero[4][2] + tablero[4][4]
+    vertical1 = tablero[0][0] + tablero[2][0] + tablero[4][0]
+    vertical2 = tablero[0][2] + tablero[2][2] + tablero[4][2]
+    vertical3 = tablero[0][4] + tablero[2][4] + tablero[4][4]
+    diagonal_derecha = tablero[0][0] + tablero[2][2] + tablero[4][4]
+    diagonal_izquierda = tablero[4][0] + tablero[2][2] + tablero[0][4]
+    if horizontal1 == 'X X X ' or horizontal2 == 'X X X ' or horizontal3 == 'X X X ' or horizontal1 == 'O O O ' or horizontal1 == 'O O O ' or horizontal3 == 'O O O ' :
+        print(f"El ganador es el jugador {numero_jugador}!!!")
+        return True
+    elif vertical1 == 'X X X ' or vertical2 == 'X X X ' or vertical3 == 'X X X ' or vertical1 == 'O O O ' or vertical3 == 'O O O ' or vertical3 == 'O O O ' :
+        print(f"El ganador es el jugador {numero_jugador}!!!")
+        return True
+    elif diagonal_derecha == 'X X X ' or diagonal_derecha == 'O O O ' or diagonal_izquierda == 'X X X ' or diagonal_derecha == 'O O O ':
+        print(f"El ganador es el jugador {numero_jugador}!!!")
+        return True
+    elif 10 - turno == 0:
+        print("GATO")
+        return True
+       
 
 while(True):
-    os.system("cls")
-    a = inputs("which grad?: ", tiradas)
-    tiradas.append(a)
+    os.system("CLS")
     imprimir_tablero()
-    os.system("pause")
+    if selecciona_ganador():
+           break
+    tiro = inputs("\nCasilla: ", tiradas)
+    actualiza_tablero(tiro)
+    if turno % 2 != 0:
+        numero_jugador = 1
+        jugador = 'X'
+    else:
+        numero_jugador = 2
+        jugador = '0'
+    tiradas.append(tiro)
+    turno += 1
+
+    
    
     
 
--- CodeWars/Python/pruebas.py
+++ CodeWars/Python/pruebas.py
@@ -1,16 +1,16 @@
-tablero = [['1 ', '| ', '2 ', '| ', '3'],
-           ['- ','- ' ,'- ', '- ' ,'- '], 
-           ['4 ', '| ', '5 ', '| ', '6'],
-           ['- ','- ' ,'- ', '- ' ,'- '], 
-           ['7 ', '| ', '8 ', '| ', '9']]
-jugador = 1
-turno = 2
-print(f"turno del jugador: {jugador}",end='')
-print(f"\t\tturno {turno}, restantes {9-turno}", end='')
-print("\n\n\n")
-for i in range(0, 5):
-    for j in range(0, 5):
-        print(tablero[i][j], end='')
-    print()
+def cero():
+    return 'cero'
 
-entrada = int(input("tirale mamada"))
+def uno():
+    return 'uno'
+
+def indirect(i):
+    switcher = {
+        0:zero,
+        1:one,
+        2:lambda:'two'
+    }
+    func=switcher.get(i, lambda : 'invalid')
+    return func()
+
+indirect(0)
